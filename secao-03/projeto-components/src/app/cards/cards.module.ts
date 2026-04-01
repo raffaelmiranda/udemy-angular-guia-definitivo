b9ea90080f1c001c974afdd8749cf94a28e8f1ee
Rafael Miranda <raffael.miranda@outlook.com.br>
2026-04-01 09:09:29 -0300

    angular

--- secao-03/projeto-components/src/app/cards/cards.module.ts
+++ secao-03/projeto-components/src/app/cards/cards.module.ts
@@ -5,6 +5,7 @@ import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
 import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { CardButtonCancelComponent } from '../card-button-cancel/card-button-can
     CardRoxoButtonComponent,
     CardButtonCancelComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, MatSliderModule],
   exports: [CardComponent, CardRoxoComponent],
 })
 export class CardsModule {}
